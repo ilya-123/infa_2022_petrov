5cd82d35107c6616bab8910c2507834a80bccfca
Petrov Ilya <petro.ily123@mail.ru>
2022-11-03 02:12:38 +0300

    Doc

--- lab8/game.py
+++ lab8/game.py
@@ -3,22 +3,21 @@ import numpy as np
 from pygame.draw import *
 from random import randint, random
 
-name = str()
 pygame.init()
 screen_x = 1400
-screen_y = 800
+screen_y = 800  # размеры игровой области
 text_x = 50
-text_y = 40
-wall = 100
-balls = list()
-number_of_balls = 5
-max_speed = 15
+text_y = 40  # Координаты счётчика очков
+wall = 100  # Толщина стен
+balls = list()  # Здесь хранятся генерируемые шарики
+number_of_balls = 5  # Кол-во шариков
+max_speed = 15  # Максимально допустимая скорость шарика
 restart_screen_x = 110
-restart_screen_y = 70
+restart_screen_y = 70  # Размеры кнопки "начать игру заново"
 time = 0
 time_x = 650
-time_y = 40
-flag = False
+time_y = 40  # Координаты таймера
+flag = False  # Флаг для кнопки "рестарт"
 
 FPS = 30
 WHITE = (255, 255, 255)
@@ -32,15 +31,15 @@ BLACK = (0, 0, 0)
 COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN]
 
 screen = pygame.display.set_mode((screen_x, screen_y), pygame.RESIZABLE)
-# table = pd.read_csv("tabular.csv")
-f = pygame.font.Font('minecraft.ttf', 24)
+f = pygame.font.Font('minecraft.ttf', 24)  # Шрифт в стиле майнкрафта
 restart_im = pygame.image.load("restart.png").convert_alpha()
-restart_im = pygame.transform.scale(restart_im, (restart_screen_x, restart_screen_y))
+restart_im = pygame.transform.scale(restart_im, (restart_screen_x, restart_screen_y))  # Иконка кнопки "рестарт"
 
 
 class Ball:
 
     def __init__(self):
+        ''' Параметры генерируемого шарика '''
         self.r = randint(20, 30)
         self.x = randint(wall + self.r, screen_x - wall - self.r)
         self.y = randint(wall + self.r, screen_y - wall - self.r)
@@ -49,13 +48,16 @@ class Ball:
         self.color = COLORS[randint(0, 5)]
 
     def draw_ball(self):
+        ''' Рисование шарика '''
         circle(screen, self.color, (self.x, self.y), self.r)
 
     def need_for_speed(self):
+        ''' Изменение координаты шарика '''
         self.x += self.v_x
         self.y += self.v_y
 
     def wall_check(self):
+        ''' Проверка столконовения шарика со стенами '''
         if (self.x + self.v_x + self.r > screen_x - wall) or (self.x + self.v_x - self.r < wall):
             self.v_x = -random() * max_speed * np.sign(self.v_x)
             self.v_y = random() * max_speed * ((-1) ** randint(-5, 5))
@@ -64,12 +66,14 @@ class Ball:
             self.v_x = random() * max_speed * ((-1) ** randint(-5, 5))
 
     def click(self):
+        ''' Проверка клика пользователя на шарик '''
         global balls
         if (event.pos[0] - self.x) ** 2 + (event.pos[1] - self.y) ** 2 <= self.r ** 2:
             del balls[i]
 
 
 def restart_check():
+    ''' Проверка нажатия пользователя на кнопку рестарт '''
     global flag
     if (screen_x - restart_screen_x < event.pos[0] < screen_x) \
             and (0 < event.pos[1] < restart_screen_y):
@@ -81,21 +85,24 @@ clock = pygame.time.Clock()
 finished = False
 
 for i in range(number_of_balls):
-    balls.append(Ball())
+    balls.append(Ball())  # Первичная генерация шариков. Создание в списке "balls" элементов из класса "Ball"
+# Начало основного цикла:
 while not finished:
     if len(balls) == 0:
-        finished = True
-    if flag:
+        finished = True  # Проверка обнуления списка шариков
+    if flag:  # Срабатывает при нажатии пользователя на кнопку "рестарт"
         balls.clear()
         time = 0
         flag = False
         for i in range(number_of_balls):
             balls.append(Ball())
+    # Рисование стен и фона кнопки "рестарт":
     polygon(screen, WHITE,
             [(wall, wall), (screen_x - wall, wall), (screen_x - wall, screen_y - wall), (wall, screen_y - wall)], 3)
     polygon(screen, WHITE,
             [(screen_x, 0), (screen_x, restart_screen_y), (screen_x - restart_screen_x, restart_screen_y),
              (screen_x - restart_screen_x, 0)])
+    # Прорисовка шариков:
     for i in range(len(balls)):
         balls[i].draw_ball()
     for event in pygame.event.get():
@@ -104,45 +111,47 @@ while not finished:
         elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
             restart_check()
             for i in range(len(balls) - 1, -1, -1):
-                balls[i].click()
-
+                balls[i].click()  # Проверка клика пользователя на шарик + на кнопку рестарт
     for i in range(len(balls)):
         balls[i].wall_check()
-        balls[i].need_for_speed()
+        balls[i].need_for_speed()  # Смещение шарика и проверка его налёта на стены
     sc_text = f.render(f'Ваш счёт: {number_of_balls - len(balls)}', True, WHITE)
     screen.blit(sc_text, (text_x, text_y))
     screen.blit(restart_im, (screen_x - restart_screen_x, 0))
     counter = f.render(f'Время: {int(time)}', True, WHITE)
-    screen.blit(counter, (time_x, time_y))
+    screen.blit(counter, (time_x, time_y))  # Вывод на экран счётчика, таймера, кнопки рестарт
     pygame.display.update()
-    time += 1 / FPS
+    time += 1 / FPS  # Отсчёт времени
     screen.fill(BLACK)
     clock.tick(FPS)
 
+''' После окончания основной игры просим пользователя ввести своё имя и сохраняем его результат в текстовый документ'''
 screen.fill(BLACK)
-screen.blit(f.render("Введите своё имя: ", True, RED), (screen_x / 3, screen_y / 3))
+screen.blit(f.render("Введите своё имя: ", True, WHITE), (screen_x / 4, screen_y / 2))
+screen.blit(f.render(f"Ваше время прохождения: {time}, c", True, WHITE), (screen_x / 4, screen_y / 3 - 40))
 pygame.display.update()
 finished = False
 name = ""
 dict_records = {}
 while not finished:
-
     screen.fill(BLACK)
-    screen.blit(f.render("Введите своё имя:", True, RED), (screen_x / 3, screen_y / 3))
+    screen.blit(f.render("Введите своё имя:", True, WHITE), (screen_x / 4, screen_y / 2))
+    screen.blit(f.render(f"Ваше время прохождения: {time}, c", True, WHITE), (screen_x / 4, screen_y / 3 - 40))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             finished = True
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_RETURN:
-                finished = True
+                finished = True     # При нажатии на enter цикл взаимодействия с пользователем прекращается
             elif event.key == pygame.K_BACKSPACE:
-                name = name[:-1]
+                name = name[:-1]    # При нажатии на backspace: стирает элемент в введёном имени, всё как и должно быть
             else:
-                name += event.unicode
-        screen.blit(f.render(name, True, RED), (screen_x / 2, screen_y / 2))
+                name += event.unicode   # Любая другая клавиша
+        screen.blit(f.render(name, True, WHITE), (screen_x / 2, screen_y / 2))
+        screen.blit(f.render(f"Ваше время прохождения: {time}, c", True, WHITE), (screen_x / 4, screen_y / 3 - 40))
         pygame.display.update()
+''' Наконец, добавляем и сохраняем результат в файле '''
 dict_records[name] = time
-print(dict_records)
 with open('records.txt', 'a') as file:
     for key, value in dict_records.items():
         file.write(f'{key}, {value}\n')
